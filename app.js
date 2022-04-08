71a95500adc497b607e5d52298c64e96fdeb005d
wesujs <93000934+wesujs@users.noreply.github.com>
2022-04-08 11:39:50 -0400

    heroku launch trial

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/", function (req, res) {
     res.render('home')
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000.");
 });
 
